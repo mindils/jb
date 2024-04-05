e8c57f913c8272e8f33a523d4fca2b63777b8a1e
LeontyevAS <leontyevas@railfgk.ru>
2024-04-05 18:13:52 +0300

    добавил рест синхронизацию

--- service/src/main/java/ru/mindils/jb/service/service/VacancySyncService.java
+++ service/src/main/java/ru/mindils/jb/service/service/VacancySyncService.java
@@ -1,11 +1,14 @@
 package ru.mindils.jb.service.service;
 
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.springframework.stereotype.Service;
+import ru.mindils.jb.service.dto.VacancySyncStatusDto;
+import ru.mindils.jb.sync.entity.VacancySyncExecution;
 import ru.mindils.jb.sync.entity.VacancySyncStep;
 import ru.mindils.jb.sync.service.VacancySyncExecutionService;
 
@@ -25,5 +28,25 @@ public class VacancySyncService {
 
   public boolean isSyncRunning() {
     return vacancySyncExecutionService.getRunningJob().isPresent();
+  }
+
+  public VacancySyncStatusDto getRunningStatus() {
+    Optional<VacancySyncExecution> runningJob = vacancySyncExecutionService.getRunningJob();
+
+    if (runningJob.isPresent()) {
+      return VacancySyncStatusDto.builder()
+          .statusText("Running job: " + runningJob.get().getStep())
+          .isSyncRunning(true)
+          .progress((runningJob.get().getStep().ordinal() + 1) * 20)
+          .step(runningJob.get().getStep().ordinal())
+          .build();
+    } else {
+      return VacancySyncStatusDto.builder()
+          .isSyncRunning(false)
+          .statusText("No running jobs")
+          .progress(100)
+          .step(0)
+          .build();
+    }
   }
 }
